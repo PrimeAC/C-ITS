b3d831f63d369d6b9fe1b8ec3faf5099e31540b6
brunoaosantos <bruno_scp5@hotmail.com>
2018-03-25 20:39:04 +0100

    remove unnecessary code

--- car.py
+++ car.py
@@ -13,21 +13,17 @@ class Receiver(threading.Thread):
 		self.port = PORT
 		self.bufsize = 1024
 		group_bin = socket.inet_pton(socket.AF_INET6, 'ff02::0')
-		mreq = group_bin + struct.pack('@I', 0) 
+		mreq = group_bin + struct.pack('@I', SCOPEID) 
 
-		self.socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-		self.socket.bind(('', PORT))
-		#ServerSock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-		#ServerSock.bind(('', PORT))
+		self.serverSocket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+		self.serverSocket.bind(('', PORT))
 		print("Server listening for messages on port: " + str(PORT))
-		#ServerSock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)	
-		self.socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
+		self.serverSocket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
 
 	def run(self):
 		while True:
 			time.sleep(5)
-			#(ClientMsg, (ClientIP, ClientPort, ClientFlow, ClientScopeId)) = ServerSock.recvfrom (self.bufsize)
-			(ClientMsg, (ClientIP, ClientPort, ClientFlow, ClientScopeId)) = self.socket.recvfrom (self.bufsize)
+			(ClientMsg, (ClientIP, ClientPort, ClientFlow, ClientScopeId)) = self.serverSocket.recvfrom (self.bufsize)
 			if not ClientMsg:
 				continue
 			print("Message: [" + ClientMsg.decode('utf-8') + "] received on IP/PORT: [" + ClientIP + "," + str(ClientPort) + "]")
@@ -40,13 +36,11 @@ class Sender(threading.Thread):
 		self.port = PORT
 		self.bufsize = 1024
 
-		ttl_bin = struct.pack('@i', 3)
-		self.dest_addr = 'ff02::0' #input("Enter destination IP Address: ")
+		ttl_bin = struct.pack('@i', SCOPEID)
+		self.dest_addr = 'ff02::0' 
 
-		self.socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-		self.socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, ttl_bin)
-		#ClientSock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-		#ClientSock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, ttl_bin)
+		self.clientSocket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+		self.clientSocket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, ttl_bin)
 
 	def run(self):
 		while True:
@@ -55,8 +49,7 @@ class Sender(threading.Thread):
 			if not message:
 				continue
 			print("Sending message [" + message + "] to " + self.dest_addr)
-			self.socket.sendto(message.encode(), (self.dest_addr, PORT, 0, SCOPEID))
-			#ClientSock.sendto(message.encode(), (self.dest_addr, PORT, 0, SCOPEID))
+			self.clientSocket.sendto(message.encode(), (self.dest_addr, PORT, 0, SCOPEID))
 			print("Message sent!")
 
 
